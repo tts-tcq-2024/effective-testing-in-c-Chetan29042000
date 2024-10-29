eada6b57828840d7585ec03b49c2ed4cac553e5b
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-10-29 14:26:50 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -3,11 +3,11 @@
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
+    if (cms <= 38) { // Include 38 in 'S' size
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if (cms > 38 && cms <= 42) { // Include 42 in 'M' size
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else {
         sizeName = 'L';
     }
     return sizeName;
@@ -15,7 +15,9 @@ char size(int cms) {
 
 int main() {
     assert(size(37) == 'S');
+    assert(size(38) == 'S'); // New test case for edge
     assert(size(40) == 'M');
+    assert(size(42) == 'M'); // New test case for edge
     assert(size(43) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
